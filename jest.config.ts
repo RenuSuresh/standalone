1276383e024870accbdd47e0eea30c26b12cf540
RenuPE101 <renu.ganvir@pharmeasy.in>
2024-11-19 15:51:21 +0530

    push

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'my-nx-project',
+  preset: './jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: './coverage/my-nx-project',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
---
+++ src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="my-nx-project" />
+    </div>
+  );
+}
+
+export default App;
